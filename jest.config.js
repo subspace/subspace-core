bd7ea7bc5222ba4e4c9a19195da6d5e7e86d1b83
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2019-08-25 23:38:56 +0300

    Remove ts-jest entirely

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,9 @@
-
 module.exports = {
   "roots": [
     "<rootDir>/dist"
   ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
-  },
   "testRegex": "(\\.|/)(test|spec)\\.js$",
   "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
+    "js"
   ],
-}
+};
