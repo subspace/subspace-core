8e5ec3562832fe4d79d7490145cc4485d62c36b8
Jeremiah Wagstaff <jeremiah.wagstaff@gmail.com>
2019-09-07 01:18:49 -0700

    improved cli

--- src/main/cli.ts
+++ src/main/cli.ts
@@ -10,30 +10,28 @@ const version = JSON.parse(fs.readFileSync(__dirname + '/../../package.json', 'u
 const program = new commander.Command();
 
 program
-  .command('run <nodeMode> <chainCount> <farmMode> <plotCount> <farmSize> <validateRecords> <encodingRounds> <storageDirectory> <resetStorage>')
+  .command('run')
   .description('Start a new node for the Subspace Protocol')
-  .action((
-    nodeMode = 'full',
-    chainCount = 1,
-    farmMode = 'disk',
-    plotCount = 100,
-    farmSize = 1000000,
-    validateRecords = true,
-    encodingRounds = 3,
-    storageDirectory,
-    resetStorage = true,
-  ) => {
-     run(nodeMode, chainCount, farmMode, plotCount, farmSize, validateRecords, encodingRounds, storageDirectory, resetStorage);
-  });
-
-  // .option('-m, --mode', 'mode to operate node (full, farmer, validator, client')
-  // .option('-c, --chains', 'number of chains in the ledger')
-  // .option('-f, --farm', 'mode to operate farm (memory, disk)')
-  // .option('-p, --plots', 'number of plots in the farm')
-  // .option('-s, --size', 'size of farm in bytes')
-  // .option('-v, --validate', 'if to validate records (t/f)')
-  // .option('-e, --encoding', 'number of rounds for piece encoding/decoding')
-  // .option('-d, --directory', 'directory for persistent storage and plotting');
+  .action((args) => {
+    run(
+      args.mode ? args.mode : 'full',
+      args.chains ? args.chains : 1,
+      args.farm ? args.farm : 'disk',
+      args.plots ? args.plots : 100,
+      args.size ? args.size : 10000000000,
+      args.validate ? args.validate : false,
+      args.encoding ? args.encoding : 3,
+      args.directory ? args.directory : null,
+    );
+  })
+  .option('-m, --mode', 'mode to operate node (full, farmer, validator, client')
+  .option('-c, --chains', 'number of chains in the ledger')
+  .option('-f, --farm', 'mode to operate farm (memory, disk)')
+  .option('-p, --plots', 'number of plots in the farm')
+  .option('-s, --size', 'size of farm in bytes')
+  .option('-v, --validate', 'if to validate records (t/f)')
+  .option('-e, --encoding', 'number of rounds for piece encoding/decoding')
+  .option('-d, --directory', 'directory for persistent storage and plotting');
 
 program
     .version(version, '-v, --version')
